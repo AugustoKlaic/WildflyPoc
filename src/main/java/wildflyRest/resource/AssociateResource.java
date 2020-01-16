346a2655cdeb2e73c13c1d374099e848d10d6b65
augusto.klaic <augusto.klaic@thomsonreuters.com>
2020-01-16 20:17:02 -0300

    Added resource class

--- src/main/java/wildflyRest/resource/AssociateResource.java
+++ src/main/java/wildflyRest/resource/AssociateResource.java
@@ -1,14 +1,23 @@
 package wildflyRest.resource;
 
+import wildflyRest.dao.AssociateDao;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/voters")
-public class AssociateResorce {
+@Produces(MediaType.APPLICATION_JSON)
+public class AssociateResource {
+
+    private AssociateDao associateDao = new AssociateDao();
+
 
     @GET
-    public Response getAllAssociates(){
-        return Response.ok().entity("\"teste\" : \"ok\"").build();
+    public Response getAllAssociates() {
+        return Response.ok().entity(associateDao.getAllAssociates()).build();
     }
 }
