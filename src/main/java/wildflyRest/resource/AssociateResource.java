bd7679b994e647730944509d2c30773412aa5ac3
augusto.klaic <augusto.klaic@thomsonreuters.com>
2020-01-20 23:17:19 -0300

    Added the Get all and the get by id in the resource

--- src/main/java/wildflyRest/resource/AssociateResource.java
+++ src/main/java/wildflyRest/resource/AssociateResource.java
@@ -1,23 +1,46 @@
 package wildflyRest.resource;
 
-import wildflyRest.dao.AssociateDao;
+import wildflyRest.entity.AssociateEntity;
+import wildflyRest.service.AssociateService;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Path("/voters")
 @Produces(MediaType.APPLICATION_JSON)
 public class AssociateResource {
 
-    private AssociateDao associateDao = new AssociateDao();
-
+    private AssociateService associateService = new AssociateService();
 
     @GET
     public Response getAllAssociates() {
-        return Response.ok().entity(associateDao.getAllAssociates()).build();
+        final List<AssociateEntity> associates = associateService.getAllAssociates();
+        return  Response.status(Response.Status.OK).entity(associates).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getAssociate(@PathParam("id") String id) {
+        final UUID associateId;
+
+        try {
+            associateId = UUID.fromString(id);
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("{\"code\" : 400, \"message\" : \"Wrong format of UUID\"}").build();
+        }
+
+        final Optional<AssociateEntity> associate = associateService.getAssociate(associateId);
+        if (associate.isPresent()) {
+            return Response.status(Response.Status.OK).entity(associate.get()).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).entity("{\"code\" : 404, \"message\" : \"No Associates found with this Id\"}").build();
+        }
     }
 }
