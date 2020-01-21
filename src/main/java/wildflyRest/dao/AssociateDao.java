21e48539f11bbbc3adc9ad37abc59d18ab50ffc9
augusto.klaic <augusto.klaic@thomsonreuters.com>
2020-01-20 22:36:54 -0300

    Added get single Associate in Dao

--- src/main/java/wildflyRest/dao/AssociateDao.java
+++ src/main/java/wildflyRest/dao/AssociateDao.java
@@ -2,10 +2,13 @@ package wildflyRest.dao;
 
 import wildflyRest.entity.AssociateEntity;
 
-import javax.inject.Inject;
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.UUID;
 
 public class AssociateDao {
 
@@ -20,7 +23,13 @@ public class AssociateDao {
     @Transactional
     public List<AssociateEntity> getAllAssociates() {
         Query query = entityManager.createQuery("SELECT a FROM AssociateEntity a");
-
         return (List<AssociateEntity>) query.getResultList();
+    }
+
+    @Transactional
+    public AssociateEntity getAssociate(UUID id) {
+        Query query = entityManager.createQuery("SELECT a from AssociateEntity a WHERE a.id = :id");
+        query.setParameter("id", id);
+        return (AssociateEntity) query.getSingleResult();
     }
 }
