da82f8e5908335898fedeb5fc7a39fa1f267f7d0
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-18 23:20:24 -0300

    Added not null validation to postgres session script

--- databaseFiles/session.sql
+++ databaseFiles/session.sql
@@ -1,7 +1,7 @@
 create table session(
     session_id uuid not null,
     session_agenda uuid not null,
-    session_duration numeric default 1,
+    session_duration numeric not null default 1,
     session_creation_time date not null,
 
     primary key (session_id),
