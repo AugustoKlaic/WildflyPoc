2ec5d97937172fb48179c8c7f71c8c6ad6850564
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-19 21:44:50 -0300

    Changed session creation time type to have the time not only the date

--- databaseFiles/session.sql
+++ databaseFiles/session.sql
@@ -2,7 +2,7 @@ create table session(
     session_id uuid not null,
     session_agenda uuid not null,
     session_duration numeric not null default 1,
-    session_creation_time date not null,
+    session_creation_time timestamp not null,
 
     primary key (session_id),
     constraint agenda_session foreign key (session_agenda) references agenda(agenda_id)
