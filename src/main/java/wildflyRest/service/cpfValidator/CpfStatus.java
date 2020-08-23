65a0ebcbfaad3e312a084a63d22006d726062b22
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-23 19:44:36 -0300

    Added CPF validator service

---
+++ src/main/java/wildflyRest/service/cpfValidator/CpfStatus.java
@@ -0,0 +1,25 @@
+package wildflyRest.service.cpfValidator;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CpfStatus {
+
+    @JsonProperty("status")
+    private String status;
+
+    public CpfStatus(String cpfStatus) {
+        this();
+        this.status = cpfStatus;
+    }
+
+    public CpfStatus() {
+    }
+
+    public String getCpfStatus() {
+        return status;
+    }
+
+    public void setCpfStatus(String cpfStatus) {
+        this.status = cpfStatus;
+    }
+}
