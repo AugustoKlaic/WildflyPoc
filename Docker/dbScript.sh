d7f5745f7deefe4f8a5d4b78055db3ae438e290d
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-27 00:07:47 -0300

    Removed the dbScript not needed anymore, and corrected the upScript

--- Docker/dbScript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t postgres-voterdb -f ../databaseFiles/Docker/Dockerfile .
-
-docker run -d -p 5433:5432 --name voter-container postgres-voterdb
--- upScript.sh
+++ upScript.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd databaseFiles || exit
-./dbScript.sh
+cd Docker || exit
+docker-compose up
+
 cd ..
+wget https://download.jboss.org/wildfly/16.0.0.Final/wildfly-16.0.0.Final.tar.gz
+
+tar xf wildfly-16.0.0.Final.tar.gz
+rm -rf wildfly-16.0.0.Final.tar.gz
+
 ./gradlew war
-
-#wget https://download.jboss.org/wildfly/16.0.0.Final/wildfly-16.0.0.Final.tar.gz
-
-#tar xf wildfly-16.0.0.Final.tar.gz
-#rm -rf wildfly-16.0.0.Final.tar.gz
-
 cp build/libs/*.war wildfly-16.0.0.Final/standalone/deployments
 echo "done copying war"
 
