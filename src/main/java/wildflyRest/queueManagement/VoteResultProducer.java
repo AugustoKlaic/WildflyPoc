c3eb116dd773a81a539a78f220533eef2e298910
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-27 23:23:44 -0300

    Create example class to sendo message to queue

--- src/main/java/wildflyRest/queueManagement/VoteResultProducer.java
+++ src/main/java/wildflyRest/queueManagement/VoteResultProducer.java
@@ -1,4 +1,30 @@
 package wildflyRest.queueManagement;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
 public class VoteResultProducer {
+
+    private static final String QUEUE_NAME = "voteResultQueue";
+
+    public void sendResultToQueue() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        factory.setUsername("voter");
+        factory.setPassword("v0t3r");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+        } catch (TimeoutException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
