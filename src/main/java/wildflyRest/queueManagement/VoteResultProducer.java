8f8fa628daa82b6f24944dc47a9c1a9785d6de6a
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-28 00:15:36 -0300

    Corrected rabbitMQ sender class

--- src/main/java/wildflyRest/queueManagement/VoteResultProducer.java
+++ src/main/java/wildflyRest/queueManagement/VoteResultProducer.java
@@ -1,8 +1,10 @@
 package wildflyRest.queueManagement;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import wildflyRest.vote.VoteResultOutput;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -12,16 +14,22 @@ public class VoteResultProducer {
 
     private static final String QUEUE_NAME = "voteResultQueue";
     private static final String EXCHANGE_NAME = "voteResultExchange";
+    private static final String HOST = "localhost";
+    private static final Integer PORT = 5672;
+    private static final String USER = "voter";
+    private static final String PASSWORD = "v0t3r";
 
-    public void sendResultToQueue() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public void sendResultToQueue(VoteResultOutput voteResultOutput) {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setPort(5672);
-        factory.setUsername("voter");
-        factory.setPassword("v0t3r");
+        factory.setHost(HOST);
+        factory.setPort(PORT);
+        factory.setUsername(USER);
+        factory.setPassword(PASSWORD);
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            String message = "Hello World!";
+            String message = objectMapper.writeValueAsString(voteResultOutput);
             channel.basicPublish(EXCHANGE_NAME, QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
         } catch (TimeoutException | IOException e) {
             e.printStackTrace();
