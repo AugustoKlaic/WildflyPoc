614e2efa29d0d5d1caabb4a5fea102b200e919b7
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-29 14:27:48 -0300

    Added private to objectmapper variable

--- src/main/java/wildflyRest/queueManagement/VoteResultProducer.java
+++ src/main/java/wildflyRest/queueManagement/VoteResultProducer.java
@@ -19,7 +19,7 @@ public class VoteResultProducer {
     private static final String USER = "voter";
     private static final String PASSWORD = "v0t3r";
 
-    ObjectMapper objectMapper = new ObjectMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     public void sendResultToQueue(VoteResultOutput voteResultOutput) {
         ConnectionFactory factory = new ConnectionFactory();
