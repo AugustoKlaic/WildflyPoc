dab208b8a621e9262dd91ec2d01335bb3e9f7c89
AugustoKlaic <gutosopelsa@hotmail.com>
2020-08-29 14:27:08 -0300

    changed session table to store an status and changed the duration tipe to be timestamp

--- Docker/postgres/databaseFiles/session.sql
+++ Docker/postgres/databaseFiles/session.sql
@@ -1,8 +1,9 @@
 create table session(
     session_id uuid not null,
     session_agenda uuid not null,
-    session_duration integer not null default 1,
+    session_duration timestamp not null default (now() + INTERVAL '1 min'),
     session_creation_time timestamp not null,
+    session_status boolean not null,
 
     primary key (session_id),
     constraint agenda_session foreign key (session_agenda) references agenda(agenda_id)
