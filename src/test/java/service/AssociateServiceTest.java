d4a95509e69e891f7c65081dffbd35c82996ec7c
AugustoKlaic <gutosopelsa@hotmail.com>
2020-09-02 22:15:13 -0300

    Added structure for unit tests

---
+++ src/test/java/service/AssociateServiceTest.java
@@ -0,0 +1,20 @@
+package service;
+
+import org.junit.Before;
+import org.junit.Test;
+import wildflyRest.associate.AssociateService;
+
+public class AssociateServiceTest {
+
+    private AssociateService associateService;
+
+    @Before
+    public void setup() {
+        this.associateService = new AssociateService();
+    }
+
+    @Test
+    public void testIfInsertAssociate() {
+        
+    }
+}
