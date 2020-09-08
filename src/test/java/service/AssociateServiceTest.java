f6762a00d91cc6f85aace9678a1815b0df88d64b
AugustoKlaic <gutosopelsa@hotmail.com>
2020-09-07 21:54:30 -0300

    added more tests to associate

--- src/test/java/service/AssociateServiceTest.java
+++ src/test/java/service/AssociateServiceTest.java
@@ -9,6 +9,9 @@ import wildflyRest.associate.AssociateDao;
 import wildflyRest.associate.AssociateEntity;
 import wildflyRest.associate.AssociateService;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
@@ -31,8 +34,16 @@ public class AssociateServiceTest {
         final AssociateEntity associateEntity = createAssociate();
         when(associateDao.insertAssociate(any(AssociateEntity.class))).thenReturn(associateEntity.getCpf());
 
-
         assertEquals(associateEntity.getCpf(), associateService.insertAssociate(associateEntity));
+    }
+
+    @Test
+    public void testIfGetAllAssociates(){
+        final List<AssociateEntity> associates = Arrays.asList(createAssociate(), createAssociate());
+        when(associateDao.getAllAssociates()).thenReturn(associates);
+
+        assertEquals(2, associateService.getAllAssociates().size());
+        assertEquals(createAssociate().getCpf(), associates.get(0).getCpf());
     }
 
     private AssociateEntity createAssociate() {
