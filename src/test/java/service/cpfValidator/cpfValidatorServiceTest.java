358bb86f0e230bf36e946c1dce6583c3c845e04b
AugustoKlaic <gutosopelsa@hotmail.com>
2020-09-12 02:16:45 -0300

    Finalized integration test

--- src/test/java/service/cpfValidator/cpfValidatorServiceTest.java
+++ src/test/java/service/cpfValidator/cpfValidatorServiceTest.java
@@ -1,18 +1,25 @@
 package service.cpfValidator;
 
-import org.junit.Test;
-
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.test.framework.AppDescriptor;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class cpfValidatorServiceTest extends JerseyTest {
 
-    private static final String URL = "/users/";
+    private static final String URL = "https://user-info.herokuapp.com/users/";
 
     @Test
     public void testCpfValidationCall() {
-
+        final String cpf = "86101153053";
+        WebResource webResource = client().resource(URL).path(cpf);
+        ClientResponse response = webResource.get(ClientResponse.class);
+        assertEquals(200, response.getStatus());
+        assertEquals(URL + cpf, webResource.toString());
     }
 
     @Override
